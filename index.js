37f2426e41e0ca6f0b0892687fd806759e08617a
Lily Keeling <lily.keeling@gmail.com>
2019-08-10 11:29:18 +0100

    function progress

--- index.js
+++ index.js
@@ -17,9 +17,43 @@ const init = () => {
     squares.push(square)
     square.dataset.row = Math.floor(i/15) + 1
     square.dataset.column = i % 15 + 1
-    
+
 
   }
+  
+
+
+  const answer = document.createElement('h6')
+  answer.innerHTML = 'TETRIS'
+  const letters = answer.innerHTML.split('')
+  console.log(letters)
+  for(let i = 5; i < 11; i ++) {
+    document.querySelector(`[data-index="${i}"]`).append(letters[i-5])
+
+  }
+
+
+  answer.innerHTML = 'beatlemaniacs'
+  const word = answer.innerHTML.split('')
+  console.log(word)
+  for(let i = 120; i < 133; i ++) {
+    document.querySelector(`[data-index="${i}"]`).append(word[i-120])
+
+  }
+
+  answer.innerHTML = 'snake'
+  const snake = answer.innerHTML.split('')
+  console.log(snake)
+  for(let i = 10; i < 85; i +=15) {
+    console.log(i)
+    console.log(i/15-1)
+    document.querySelector(`[data-index="${i}"]`).append(snake[Math.floor(i/15)])
+
+
+  }
+
+
+
 
 
 
@@ -48,3 +82,36 @@ const init = () => {
 
 
 window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+// answer.innerHTML = 'snake'
+// const snake = answer.innerHTML.split('')
+// console.log(snake)
+// for(let i = 10; i < 85; i +=15) {
+//   console.log(i)
+//   document.querySelector(`[data-index="${i}"]`).append(snake[i-10])
+//   console.log(snake[i-15-25])
+//
+// }
+// for(let j = 0; j < snake.length; j++){
+//   console.log(snake[j])
+//   console.log(i)
+//   console.log(document.querySelector(`[data-index="${i}"]`).append(snake[i][j]))
+//
+//
+// }
+
+
+// answer.innerHTML = 'snake'
+// const snake = answer.innerHTML.split('')
+// console.log(snake)
+// for(let j = 0; j < snake.length; j++){
+//   for(let i = 10; i < 85; i +=15) {
+//     console.log(j)
+//     document.querySelector(`[data-index="${i}"]`).append(snake[j])
+//
+//
+//   }
+// }
